d8ca452b697c9928c2599781c516d790b7f11edc
“Your <you@example.com>
2022-02-03 18:12:09 +0200

    Ruokan ei-argumenttia tarkistus korjattu. Parita-komento lisätty

---
+++ commands/parita.js
@@ -0,0 +1,43 @@
+module.exports = {
+    name: 'parita',
+    description:
+        'Laittaa annetut nimet arvotuiksi pariksi.' +
+        'Esim. ..parita Konsta Joona Kissa Koira Tuukka Valtteri Lammas' +
+        '=>Joona Koira' +
+        'Valtteri Tuukka' +
+        'Konsta Kissa' +
+        'Lammas',
+    execute(message, args) {
+        if (args.length === 0) {
+            message.channel.send(
+                'Anna paritettavat. Esim: ".parita Kissa Koira"'
+            );
+            return;
+        }
+        //  Random-järkkään nimet, lähde: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+        const array = args;
+        let currentIndex = array.length,
+            randomIndex;
+        // While there remain elements to shuffle...
+        while (currentIndex != 0) {
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex],
+                array[currentIndex],
+            ];
+        }
+
+        let palautus = '';
+        let i = 1;
+        array.forEach((nimi) => {
+            let merkki = i % 2 ? '\n' : '\n\n';
+            palautus += nimi += merkki;
+            i++;
+        });
+        message.channel.send(palautus);
+    },
+};
--- commands/ruoka.js
+++ commands/ruoka.js
@@ -1,89 +1,91 @@
 const ravintolat_t = [
-  {
-    nimi: "piato",
-    url: "https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=1408&language=fi",
-  },
-  {
-    nimi: "maija",
-    url: "https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=1402&language=fi",
-  },
-  {
-    nimi: "tilia",
-    url: "https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=1413&language=fi",
-  },
-  {
-    nimi: "lozzi",
-    url: "https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=1401&language=fi",
-  },
-  {
-    nimi: "belvedere",
-    url: "https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=1404&language=fi",
-  },
-  {
-    nimi: "syke",
-    url: "https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=1405&language=fi",
-  },
-  {
-    nimi: "uno",
-    url: "https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=1414&language=fi",
-  },
-  {
-    nimi: "ylistö",
-    url: "https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=1403&language=fi",
-  },
-  {
-    nimi: "kvarkki",
-    url: "https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=140301&language=fi",
-  },
-  {
-    nimi: "rentukka",
-    url: "https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=1416&language=fi",
-  },
-  {
-    nimi: "novelli",
-    url: "https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=1409&language=fi",
-  },
-  {
-    nimi: "taide",
-    url: "https://www.foodandco.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=0301&language=fi",
-  },
-  {
-    nimi: "fiilu",
-    url: "https://www.foodandco.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=3364&language=fi",
-  },
+    {
+        nimi: 'piato',
+        url: 'https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=1408&language=fi',
+    },
+    {
+        nimi: 'maija',
+        url: 'https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=1402&language=fi',
+    },
+    {
+        nimi: 'tilia',
+        url: 'https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=1413&language=fi',
+    },
+    {
+        nimi: 'lozzi',
+        url: 'https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=1401&language=fi',
+    },
+    {
+        nimi: 'belvedere',
+        url: 'https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=1404&language=fi',
+    },
+    {
+        nimi: 'syke',
+        url: 'https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=1405&language=fi',
+    },
+    {
+        nimi: 'uno',
+        url: 'https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=1414&language=fi',
+    },
+    {
+        nimi: 'ylistö',
+        url: 'https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=1403&language=fi',
+    },
+    {
+        nimi: 'kvarkki',
+        url: 'https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=140301&language=fi',
+    },
+    {
+        nimi: 'rentukka',
+        url: 'https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=1416&language=fi',
+    },
+    {
+        nimi: 'novelli',
+        url: 'https://www.semma.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=1409&language=fi',
+    },
+    {
+        nimi: 'taide',
+        url: 'https://www.foodandco.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=0301&language=fi',
+    },
+    {
+        nimi: 'fiilu',
+        url: 'https://www.foodandco.fi/modules/MenuRss/MenuRss/CurrentDay?costNumber=3364&language=fi',
+    },
 ];
 
-let Parser = require("rss-parser");
+let Parser = require('rss-parser');
 
 module.exports = {
-  name: "ruoka",
-  description: "Komento antamaan tietyn opiskelijaravintolan ruokalista.",
-  execute(message, args) {
-    if (args.length > 1) {
-      message.channel.send("Yksi ravintola kerrallaan");
-      return;
-    }
-    let ravintola = args[0].toLowerCase();
-    let url;
-    for (let i = 0; i < ravintolat_t.length; i++) {
-      if (ravintolat_t[i].nimi === ravintola) {
-        url = ravintolat_t[i].url;
-      }
-    }
-    if (url == undefined) {
-      message.channel.send("Nyt ei pysty");
-      return;
-    }
+    name: 'ruoka',
+    description: 'Komento antamaan tietyn opiskelijaravintolan ruokalista.',
+    execute(message, args) {
+        if (args.length < 1) {
+            message.channel.send('Yksi ravintola kerrallaan');
+            return;
+        }
+        let ravintola = args[0].toLowerCase();
+        let url;
+        for (let i = 0; i < ravintolat_t.length; i++) {
+            if (ravintolat_t[i].nimi === ravintola) {
+                url = ravintolat_t[i].url;
+            }
+        }
+        if (url == undefined) {
+            message.channel.send('Nyt ei pysty');
+            return;
+        }
 
-    let parser = new Parser();
-    (async () => {
-      let feed = await parser.parseURL(url);
-      let ravintolan_nimi = feed.title;
-      let ruokalista = feed.items[0].content.replaceAll("<br>", "");
-      if (ruokalista.length == 0) {
-        ruokalista = "Ei ruokalistaa saatavilla";
-      }
-      message.channel.send("**" + ravintolan_nimi + "**" + "\n\n" + ruokalista);
-    })();
-  },
+        let parser = new Parser();
+        (async () => {
+            let feed = await parser.parseURL(url);
+            let ravintolan_nimi = feed.title;
+            let ruokalista = feed.items[0].content.replaceAll('<br>', '');
+            if (ruokalista.length == 0) {
+                ruokalista = 'Ei ruokalistaa saatavilla';
+            }
+            message.channel.send(
+                '**' + ravintolan_nimi + '**' + '\n\n' + ruokalista
+            );
+        })();
+    },
 };
