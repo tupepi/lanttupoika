4802db636f2ff1aff173039a4630693b0a0c27cc
“Your <you@example.com>
2022-02-03 14:05:47 +0200

    ping pong ping pong Tuukka toinen

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ping',
     description: 'Eka testikomento',
     execute(message, args) {
-        message.channel.send('ping pong ping pong Tuukka!');
+        message.channel.send('ping pong ping pong Tuukka toinen yritys!');
     },
 };
