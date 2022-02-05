a05b7adcf8269ef905cc7617a9f46cbdd659b518
“Your <you@example.com>
2022-02-05 16:49:54 +0200

    help-komennon parametri

--- commands/help.js
+++ commands/help.js
@@ -3,12 +3,23 @@ module.exports = {
     name: 'help',
     description: 'Listaa kaikki komennot ja niiden kuvaukset',
     execute(message, args) {
+        var komento = '';
+
+        if (args.length > 0) {
+            komento = args[0];
+        }
+
         const commandFiles = fs
             .readdirSync('./commands')
             .filter(
-                (file) => file.endsWith('.js') && !file.endsWith('help.js')
+                (file) => file.endsWith(`${komento}.js`) && file != 'help.js'
             );
+
         palautus = '';
+        if (commandFiles.length == 0) {
+            palautus = 'Komentoa ei löydy. Kokeile: "..help"';
+        }
+
         for (const file of commandFiles) {
             const command = require(`./${file}`);
             const nimi = command.name.toUpperCase();
