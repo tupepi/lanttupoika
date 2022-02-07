42153bb070faafd8052f3f634fb8aa3214766b0b
laitjjxz <joona.j.laitinen@student.jyu.fi>
2022-02-07 23:43:08 +0200

    paivat lissatty

--- commands/deadline.js
+++ commands/deadline.js
@@ -6,16 +6,24 @@ module.exports = {
     let nyt = Date.now();
     let palautus = new Date(2022, 4, 30, 23, 59, 59, 999);
     lopputulos = palautus - nyt;
-    let tunnit = Math.floor(lopputulos / 3600000);
-    let minuutit = Math.floor((lopputulos - tunnit * 3600000) / 60000);
+    let paivat = Math.floor(lopputulos / 86400000);
+    let tunnit = Math.floor(lopputulos - (paivat * 86400000) / 3600000);
+    let minuutit = Math.floor(
+      lopputulos - (paivat * 86400000 + tunnit * 3600000) / 60000
+    );
     let sekunnit = Math.floor(
-      (lopputulos - (tunnit * 3600000 + minuutit * 60000)) / 1000
+      (lopputulos - (paivat * 86400000 + tunnit * 3600000 + minuutit * 60000)) /
+        1000
     );
     let millisekunnit = Math.floor(
-      lopputulos - tunnit * 3600000 - minuutit * 60000 - sekunnit * 1000
+      lopputulos -
+        paivat * 86400000 -
+        tunnit * 3600000 -
+        minuutit * 60000 -
+        sekunnit * 1000
     );
     message.channel.send(
-      `Tunteja: ${tunnit}, Minuutteja: ${minuutit}, Sekunteja: ${sekunnit}, Millisekunteja:${millisekunnit}. `
+      `Paivia: ${paivat} , Tunteja: ${tunnit} , Minuutteja: ${minuutit} , Sekunteja: ${sekunnit} , Millisekunteja:${millisekunnit} . `
     );
   },
 };
