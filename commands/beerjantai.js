ffb308455339b3198c79d8f16eefe6d16c0eb5e1
kahtakju <konsta.kahtava@hotmail.com>
2022-02-17 16:05:27 +0200

    beerjantai-komennon lisäys ja palindromiko-komennon päivitystä

---
+++ commands/beerjantai.js
@@ -0,0 +1,48 @@
+module.exports = {
+    name: 'beerjantai',
+    description:
+        'Palauttaa kuinka monta päivää perjantaihin',
+    execute(message, args) {
+        const viikonpäivät = ["Sunnuntai", "Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai"];
+        const paiva = new Date();
+        let tanaanINT = paiva.getDay();
+        let paivaaPerjantaihin = 5 - tanaanINT
+        let tamaPaiva = viikonpäivät[tanaanINT];
+
+        if (tanaanINT !== 4) {
+            message.channel.send(`Tänään on ${tamaPaiva}. \nPerjantaihin on aikaa ${paivaaPerjantaihin} päivää.`);
+            return;
+
+        }
+
+        if (tanaanINT === 0 ) {
+
+            message.channel.send(`Tänään on ${tamaPaiva}. \nPerjantaihin on aikaa 5 päivä. Vaikka kärsisitkin sunnuntai masennuksesta niin huoli pois ja käännä
+            katse kohti tulevaa perjantaita. Se tulee nopeammin kuin kuvittelisitkaan ;)`);
+            return;
+            
+        }
+
+      
+        if (tanaanINT === 6 ) {
+
+            message.channel.send(`Tänään on ${tamaPaiva}. Perjantai oli ja meni, mutta huolipois sillä tänään on lauantai! Lauantain agendaan voi kuulua samat asiat
+            kuin perjantain. Muista nauttia tästäkin päivästä, sillä viikonloppu ei ole lähelläkään loppua.
+            \nPerjantaihin on aikaa 6 päivä`);
+            return;
+            
+        }
+
+        if (tamaPaiva === "Perjantai") {
+
+            message.channel.send(`Onneksi olkoon! Tänään on ${tamaPaiva} AKA BEERJANTAI. \nTänään alkaa viikonlopun vietto! Tarkoittaako se rennosti ottamista, 
+            oluen juomista, urheilemista tai peräti huumoripäivää? Sen saat sinä itse päättä ;).\nEnnen kaikkea muista ottaa rennosti ja ladata akut seuraavaa viikkoa 
+            varten!`
+            );
+            return;
+        }
+
+
+        message.channel.send(`Tänään on ${tamaPaiva}. \nPerjantaihin on aikaa ${paivaaPerjantaihin} päivä`);
+    },
+};
--- commands/palindromiko.js
+++ commands/palindromiko.js
@@ -1,33 +1,53 @@
 module.exports = {
   name: "palindromiko",
-  description: "Tarkistaa annettu argumentti palindromi",
+  description: "Tarkistaa annettu argumentti palindromi. Komennolla help saat lisätietoja komennon toimivuudesta",
   execute(message, args) {
 
-    //Tarkistaa onko käyttäjä ollut idiootti ja eikä tajunnut antaa argumenttia
-    if (args.length < 1) {
-      message.channel.send("Mörhöily (hörpyt pankkiin)" + " " + "Vinkki: laita yksi argumentti '..palindromiko' perään"  + " " + "Esimerkiksi: '..palindromiko mörhöily'")
-      return;
-    };
 
-    //Tarkistaa onko käyttäjä antanut useamman kuin yhden argumentin
+       //Tarkistaa onko käyttäjä ollut idiootti ja eikä tajunnut antaa argumenttia
+       if (args.length < 1) {
+        message.channel.send(`Anna argumentti!
+        \nHelp-komennolla saa lisäinformaatiota komennon toiminnasta`)
+        return;
+      };
+   
+      //Tarkistaa onko ensimmäinen argumentti "help", jos on niin tarjoaa lisäinformaatiota komennontoiminnasta.
+      if (Object.is(args[0].toLowerCase().toString(), "help")) {
+        message.channel.send(`Jos annat yhden argumentin, joka ei ole "help" niin komento tarkistaa onko kyseinen argumentti palindromi.
+        \nJos annat enemmänkuin yhden argumentin (esimerkiksi kokonaisen lauseen) niin komento tarkistaa onko annettu lause/argumentit palindromi. Ethän anna erikoismerkkejä!`)
+        return;
+
+      }
+
+
+    //Jos argumenttejä on enemmän kuin yksi niin liitetään kaikki argumentit yhteen pötköön ja tarkistetaan onko palindromi.
     if (args.length > 1) {
-      message.channel.send("Tämän hetkisessä versiossa voi tarkistaa vain yhden argumentin kerrallaan") 
-      return;
-    };
 
-    //Otetaan argumenttina annettu sana talteen ja muutetaan kaikki kirjaimet pieniksi
-    let kirjoitettuSana = args[0].toLowerCase();
+      let koottunaYhteen = args.join('').toLowerCase();
+      let kaikkipilkottu = koottunaYhteen.split("").reverse().join('').toLowerCase();
+      if (Object.is(koottunaYhteen, kaikkipilkottu)) {
+        message.channel.send("No onhan se!") 
+        return;
+      }
 
-    //Pilkotaan annettu argumentti paloiksi -> käännetään järjestys -> Yhdistetään yksittäiset kirjaimet kokonaiseksi sanaksi -> muutetaan kaikki kirjaimet pieniksi
-    let pilkottu = kirjoitettuSana.split("").reverse().join('').toLowerCase();
+    }
 
-    //Tarkistetaan onko alkuperäinen argumentti ja käännetty argumentti identtiset
-    //Jos on niin palautetaan viesti: "No onhan se!"
-    if (Object.is(kirjoitettuSana, pilkottu)) message.channel.send("No onhan se!")
+      //Otetaan argumenttina annettu sana talteen ja muutetaan kaikki kirjaimet pieniksi
+      let kirjoitettuSana = args[0].toLowerCase();
 
-    //Jos alkuperäinen argumentti ja käänteinen argumentti ole identtiset palautetaan viesti: "Eipä ollu!"
-    else message.channel.send("Eipä ollu!")
-       
+      //Pilkotaan annettu argumentti paloiksi -> käännetään järjestys -> Yhdistetään yksittäiset kirjaimet kokonaiseksi sanaksi -> muutetaan kaikki kirjaimet pieniksi
+      let pilkottu = kirjoitettuSana.split("").reverse().join('').toLowerCase();
+
+      //Tarkistetaan onko alkuperäinen argumentti ja käännetty argumentti identtiset
+      //Jos on niin palautetaan viesti: "No onhan se!"
+      if (Object.is(kirjoitettuSana, pilkottu)) {
+        message.channel.send("No onhan se!") 
+        return;
+      }
+
+      //Jos alkuperäinen argumentti ja käänteinen argumentti ole identtiset palautetaan viesti: "Eipä ollu!"
+      message.channel.send("Eipä ollu!")
+        
 
 
   },
