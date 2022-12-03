9c3e92480f1e88c6932affae2ecccdcb76470600
Tuukka Pitkänen <tuukkapitkanen2@gmail.com>
2022-12-03 18:09:21 +0200

    discordin versioon liittyvä bugi

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
 const fs = require('fs');
-const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
+const client = new Discord.Client({ intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+  ] });
 
 client.commands = new Discord.Collection();
 const commandFiles = fs
